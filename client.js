b1161ea872ea92fb8ab4042421180f224a1d2456
Mike Ackison <mike.ackison@shopify.com>
2021-01-13 19:36:08 +0000

    able to Move: up

--- client.js
+++ client.js
@@ -1,5 +1,6 @@
 const net = require('net');
 
+const name = "Name: MCA"
 // Establish connection with game server
 
 
@@ -11,6 +12,26 @@ const connect = function() {
   });
   // this iterperets incoming data as text
   conn.setEncoding('utf8');
+
+
+  // print a message to the screen when a connection is established
+  // use the event handler
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+    // write name 
+    conn.write(name);
+
+    // conn.write("Move: up")
+    
+    // setInterval(function () {
+    //   conn.write("Move: up")
+    // }, 1000);
+    
+    
+    
+});
+
+
 
   // Update the connect function to also attach an event handler
   // to handle incoming data and console log it for the player
@@ -26,6 +47,18 @@ const connect = function() {
   return conn;
 };
 
+
+
+
+
 // in curley braces, because the opject on play is in curly braces,
 // if we remove on both it will stil run at this level
 module.exports = { connect };
+
+
+
+
+// "Move: up" - move up one square (unless facing down)
+// "Move: down" - move down one square (unless facing up)
+// "Move: left" - move left one square (unless facing right)
+// "Move: right" - move left one square (unless facing left)
