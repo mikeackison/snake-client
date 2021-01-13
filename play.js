6e9a9618eeee639ece72d7e6affa74ac3f71826c
Mike Ackison <mike.ackison@shopify.com>
2021-01-13 20:48:23 +0000

    fix control C issuie, working

--- play.js
+++ play.js
@@ -4,4 +4,33 @@
 
 const { connect } = require('./client');
 console.log('Connecting...')
-connect();
+
+
+// setup user interface
+// specifically so that we can handle user input via stdin
+
+
+//Create a function handleUserInput and register it as the "data" callback handler for stdin
+
+
+const handleUserInput = ('data', (key) => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+
+});
+
+const setupInput = function() {
+  
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.on('data', handleUserInput)
+  stdin.resume();
+  
+  
+  // then return
+  return stdin;
+};
+
+setupInput(connect());
