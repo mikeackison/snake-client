22b0e58eff51c03011a01e85448b44b4563c4c7c
Mike Ackison <mike.ackison@shopify.com>
2021-01-13 22:52:17 +0000

    moved out funtions, and added a require, still working

--- play.js
+++ play.js
@@ -3,34 +3,10 @@
 
 
 const { connect } = require('./client');
+const { setupInput } = require('./index')
 console.log('Connecting...')
 
 
-// setup user interface
-// specifically so that we can handle user input via stdin
 
-
-//Create a function handleUserInput and register it as the "data" callback handler for stdin
-
-
-const handleUserInput = ('data', (key) => {
-  if (key === '\u0003') {
-    process.exit();
-  }
-
-});
-
-const setupInput = function() {
-  
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.on('data', handleUserInput)
-  stdin.resume();
-  
-  
-  // then return
-  return stdin;
-};
-
-setupInput(connect());
+connect()
+setupInput();
