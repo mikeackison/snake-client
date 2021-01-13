23d4397c374589dabe0d668b4d137da0160fded9
Mike Ackison <mike.ackison@shopify.com>
2021-01-13 18:10:52 +0000

    first commit

---
+++ play.js
@@ -0,0 +1,34 @@
+
+const net = require('net');
+
+// Establish connection with game server
+
+
+
+const connect = function() {
+    const conn = net.createConnection({
+      host: '135.23.222.131',
+      port: 50542
+
+    });
+// this iterperets incoming data as text
+conn.setEncoding('utf8');
+
+
+// Update the connect function to also attach an event handler
+// to handle incoming data and console log it for the player
+
+// message from user
+// on('data') --- do something, when Data got recieved
+// callback funtion needs to have two paramenters 'data' being one and the 
+// var data
+conn.on('data', (data) => {
+  console.log(data)
+})
+
+return conn;
+
+}
+
+console.log('Connecting...')
+connect();
