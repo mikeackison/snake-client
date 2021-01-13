981ddf31ea193d48da2b720a79c73711e2782092
Mike Ackison <mike.ackison@shopify.com>
2021-01-13 18:29:27 +0000

    move connect function to client.js. Use module.exports as an object

--- play.js
+++ play.js
@@ -1,34 +1,7 @@
 
-const net = require('net');
-
-// Establish connection with game server
+// const net = require('net');
 
 
-
-const connect = function() {
-    const conn = net.createConnection({
-      host: '135.23.222.131',
-      port: 50542
-
-    });
-// this iterperets incoming data as text
-conn.setEncoding('utf8');
-
-
-// Update the connect function to also attach an event handler
-// to handle incoming data and console log it for the player
-
-// message from user
-// on('data') --- do something, when Data got recieved
-// callback funtion needs to have two paramenters 'data' being one and the 
-// var data
-conn.on('data', (data) => {
-  console.log(data)
-})
-
-return conn;
-
-}
-
+const { connect } = require('./client');
 console.log('Connecting...')
 connect();
